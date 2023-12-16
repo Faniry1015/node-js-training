9cdd9473d9dc91f0171e907b2525f51cee2cd481
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-12-16 11:43:51 +0300

    using unlink

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { readFile, writeFile, unlink, copyFile } from'node:fs/promises' //unlink pour supprimer, autres sont évidents
+import { readFile, writeFile, unlink, copyFile } from'node:fs/promises' 
+//unlink pour supprimer, autres sont évidents / /promises peut être enlever pour ne pas utiliser de promises. Ajouter Sync (ex: readFileSync) pour utiliser des fonctions synchrones
 
 async function writeContent() {
     await writeFile('demo.txt', 'Je suis le text ajouter', {
@@ -9,5 +10,15 @@ async function writeContent() {
 writeContent()
 
 const content = await readFile('demo.txt', {encoding: 'utf8'})
-
 console.log(content)
+
+async function deletefile () {
+    try {
+        await unlink('delete.js')
+        console.log('fichier supprimer')
+    } catch(e) {
+        throw new Error('Le fichier à sans doute déjà été supprimer', console.log(e.message))
+    }
+}
+
+deletefile()
