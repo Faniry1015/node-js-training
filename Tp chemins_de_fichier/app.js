f93e7b8d0f2aba7a5103e3643d9c654c7a3d4081
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-12-17 12:33:56 +0300

    tp done

--- Tp chemins_de_fichier/app.js
+++ Tp chemins_de_fichier/app.js
@@ -1,10 +1,15 @@
-import {readdir} from 'node:fs/promises'
+import {readdir, stat} from 'node:fs/promises'
 
 //TP récuperer les contenu du dossier, lister tous les fichiers et dossiers + poids pour fichiers
 
-const currentDir = await readdir('./')
+const currentDir = await readdir('./', {withFileTypes:true})
 for (const item of currentDir) {
-    console.log(item)
+    if (item.isDirectory()) {
+        console.log('D', item.name)
+    } else {
+        const currentItem = await stat(item.name)
+        console.log('F', item.name, currentItem.size)
+    }
 }
 
 
