95b52949f813183e95eb6f359449f48c0d3377af
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-12-21 15:13:44 +0300

    tp todo list put done

--- tp-todolist/app.js
+++ tp-todolist/app.js
@@ -7,15 +7,28 @@ import { json, text } from 'node:stream/consumers'
 const server = createServer(async (req, res) => {
     const dir = dirname(fileURLToPath(import.meta.url))
     const todosPath = join(dir, 'storage/todos.json')
+    let todosArray = JSON.parse(await readFile(todosPath, 'utf8'))
+    const newTodo = await json(req)
     if(req.method === 'GET' && req.url === '/todos') {
         const todosRead = await readFile(todosPath)
         res.write(todosRead)
     } else if (req.method === 'POST' && req.url === '/todos') {
-        const newTodo = await json(req)
-        const todosArray = JSON.parse(await readFile(todosPath, 'utf8'))
         todosArray.push(newTodo)
-        await writeFile(todosPath, todosArray)
-        res.write(todosPath)
+        await writeFile(todosPath, JSON.stringify(todosArray))
+    } else if (req.method === 'PUT' && req.url.startsWith('/todos')) {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        const currentTodoId = parseInt(url.searchParams.get('id'))
+        todosArray = todosArray.map((todo) => {
+            if (todo.id === currentTodoId) {
+                return todo = {
+                    id: currentTodoId,
+                    ...newTodo
+                }
+            } else {
+                return todo
+            }
+        })
+        await writeFile(todosPath, JSON.stringify(todosArray))
     }
     res.end()
 })
