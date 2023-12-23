d3cb89d51b20c0d8587c0886a1621458915cff5b
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-12-23 08:24:37 +0300

    todo list done

--- tp-todolist/app.js
+++ tp-todolist/app.js
@@ -1,27 +1,38 @@
 import { createServer } from 'node:http'
-import { create, index, remove } from './functions/api/todos.js'
+import { create, index, remove, update } from './functions/api/todos.js'
+import { NotFoundError } from './functions/errors.js'
 
 const server = createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    const url = new URL(req.url, `http://${req.headers.host}`)
-    const endpoint = `${req.method}:${url.pathname}`
-    let results
-    switch (endpoint) {
-        case 'GET:/todos':
-            results = await index(req, res)
-            break;
-        case 'POST:/todos':
-            results = await create(req, res)
-            break;
-        case 'DELETE:/todos':
-            results = await remove(req, res, url) 
-            break;
-        default: res.writeHead(404)
+    try {
+        res.setHeader('Content-Type', 'application/json')
+        const url = new URL(req.url, `http://${req.headers.host}`)
+        const endpoint = `${req.method}:${url.pathname}`
+        let results
+        switch (endpoint) {
+            case 'GET:/todos':
+                results = await index(req, res)
+                break;
+            case 'POST:/todos':
+                results = await create(req, res)
+                break;
+            case 'DELETE:/todos':
+                results = await remove(req, res, url)
+                break;
+            case 'PUT:/todos':
+                results = await update(req, res, url)
+                break;
+            default: res.writeHead(404)
+        }
+        if (results) {
+            res.write(JSON.stringify(results))
+        }
+    } catch (e) {
+        if (e instanceof NotFoundError) {
+            res.writeHead(404)
+        } else {
+            throw e
+        }
     }
-    if (results) {
-        res.write(JSON.stringify(results))
-    }
-
     res.end()
 })
 
--- tp-todolist/functions/api/todos.js
+++ tp-todolist/functions/api/todos.js
@@ -1,19 +1,21 @@
-import { createTodo, findTodos, removeTodo } from "../todos_storage.js"
+import { createTodo, findTodos, removeTodo, updateTodo } from "../todos_storage.js"
 import { json } from 'node:stream/consumers'
 
 export async function index (req, res) {
-    const todos = await findTodos()
-    return todos
+    return await findTodos()
 }
 
 export async function create (req, res) {
-    const newTodo = await json(req)
-    const todo = await createTodo(newTodo)
-    return todo
+    return createTodo(await json(req))
 } 
 
 export async function remove (req, res, url) {
     const id = parseInt(url.searchParams.get('id'), 10)
     await removeTodo(id)
     res.writeHead(204)
+}
+
+export async function update (req, res, url) {
+    const id = parseInt(url.searchParams.get('id'), 10)
+    return  updateTodo(id, await json(req) )
 }
